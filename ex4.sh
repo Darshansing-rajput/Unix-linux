5f8bbb79990d9c4608b329609eef42854d2b7ab9
Darshansing-rajput <109502321+Darshansing-rajput@users.noreply.github.com>
2022-07-19 15:51:11 +0530

    Add files via upload

---
+++ ex4.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+if [ $# -lt 2 ]
+    then
+    echo "Error : Invalid number of arguments. Provide atleast two files as argument"
+	exit 1
+fi
+​
+# Convert reference file in to words on each line, ignore spaces.
+search_file=`tr '\n' ' ' < $1` 
+​
+
+for file_name in "$@"
+do
+
+    if [ $file_name = $1 ]
+        then 
+        continue
+    fi
+	   
+    for search_word in $search_file
+    do
+        echo "$file_name: $search_word:" $(tr ' ' '\n' <"$file_name" | grep -c "$search_word")	
+ done
+done	
